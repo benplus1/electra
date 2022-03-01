e3566b1a9b02eef50756022ecbd9041df43ffcc2
Ben Yang <yang.benjamin1998@gmail.com>
2022-03-01 00:18:38 -0500

    a

--- finetune/classification/classification_tasks.py
+++ finetune/classification/classification_tasks.py
@@ -142,7 +142,7 @@ class SingleOutputTask(task.Task):
       cls_ids.append(0)
       label_ids.append(0)
 
-    class_weights = [positive / (self.config.max_seq_length), (self.config.max_seq_length - positive) / (self.config.max_seq_length)]
+    class_weights = [positive / (self.config.max_seq_length / 4), (self.config.max_seq_length - positive) / (self.config.max_seq_length / 4)]
     tokens.append("[SEP]")
     segment_ids.append(0)
     cls_ids.append(0)
