70e956d2d5c5f2c7576bdb4d38a10666db36c84f
Ben Yang <yang.benjamin1998@gmail.com>
2022-03-03 21:39:24 -0500

    a

--- finetune/classification/classification_metrics.py
+++ finetune/classification/classification_metrics.py
@@ -89,11 +89,11 @@ class F1Scorer(SentenceLevelScorer):
       for y_true, pred, cls_id in zip(y_trues, preds, cls_ids):
         if cls_id == 1: # cls_id good
           if pred == self._positive_label:
+            n_predicted += 1
+          if y_true == self._positive_label:
             n_gold += 1
-            if pred == self._positive_label:
-              n_predicted += 1
-              if pred == y_true:
-                n_correct += 1
+          if y_true == pred and pred == self._positive_label:
+            n_correct += 1
     if n_correct == 0:
       p, r, f1 = 0, 0, 0
     else:
