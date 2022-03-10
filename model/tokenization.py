1a465b158cd83dddf74bf363cf2e46d9845d22c8
Ben Yang <yang.benjamin1998@gmail.com>
2022-03-09 21:50:52 -0500

    a

--- model/tokenization.py
+++ model/tokenization.py
@@ -351,10 +351,11 @@ def _is_punctuation(char):
   # Characters such as "^", "$", and "`" are not in the Unicode
   # Punctuation class but we treat them as punctuation anyways, for
   # consistency.
-  if ((cp >= 33 and cp <= 47) or (cp >= 58 and cp <= 64) or
-      (cp >= 91 and cp <= 96) or (cp >= 123 and cp <= 126)):
-    return True
-  cat = unicodedata.category(char)
-  if cat.startswith("P"):
-    return True
+  # MODIFIED THIS
+  # if ((cp >= 33 and cp <= 47) or (cp >= 58 and cp <= 64) or
+  #     (cp >= 91 and cp <= 96) or (cp >= 123 and cp <= 126)):
+  #   return True
+  # cat = unicodedata.category(char)
+  # if cat.startswith("P"):
+  #   return True
   return False
