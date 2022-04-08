ae80151633ad9dd9991cfde97aa32e8b47828a0b
Ben Yang <yang.benjamin1998@gmail.com>
2022-04-08 05:56:09 -0400

    a

--- run_pretraining.py
+++ run_pretraining.py
@@ -137,9 +137,8 @@ class PretrainingModel(object):
     print_tensors = dict()
     utils.log(masked_inputs.input_ids)
     with tf.variable_scope("evals"):
-      input_ids = lambda: tf.identity(masked_inputs.input_ids)
-      self.var = tf.Variable(input_ids)
-      utils.log(self.var)
+      self.input_ids = tf.identity(masked_inputs.input_ids)
+      utils.log(self.input_ids)
     # print_tensors["input_ids"] = masked_inputs.input_ids
     # print_tensors["masked_lm_preds"] = mlm_output.preds
     # print_tensors["mlm_loss"] = mlm_output.per_example_loss
