f479e5cb2b48f84ebd1698fc3a73cab0b71e226b
Ben Yang <yang.benjamin1998@gmail.com>
2022-04-08 04:40:35 -0400

    a

--- run_pretraining.py
+++ run_pretraining.py
@@ -33,39 +33,32 @@ from pretrain import pretrain_helpers
 from util import training_utils
 from util import utils
 
+from tf.metrics import _aggregate_across_replicas, metric_variable
 from tensorflow.python.framework import dtypes
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import state_ops
 
+def cringe(values):
+  with variable_scope.variable_scope(None, 'cringe', (values)):
+    values = math_ops.cast(values, dtypes.float32)
 
-# def cringe(values):
-#   with variable_scope.variable_scope(name, 'mean', (values, weights)):
-#     values = math_ops.cast(values, dtypes.float32)
+    orig_tens = metric_variable([], dtypes.float32, name='total')
+    # count = metric_variable([], dtypes.float32, name='count')
+    # num_values = math_ops.cast(array_ops.size(values), dtypes.float32)
 
-#     total = metric_variable([], dtypes.float32, name='total')
-#     count = metric_variable([], dtypes.float32, name='count')
+    update_total_op = state_ops.assign_add(orig_tens, math_ops.cast(values, dtypes.float32))
+    # with ops.control_dependencies([values]):
+    #   update_count_op = state_ops.assign_add(count, num_values)
 
-#     if weights is None:
-#       num_values = math_ops.cast(array_ops.size(values), dtypes.float32)
-#     else:
-#       values, _, weights = _remove_squeezable_dimensions(
-#           predictions=values, labels=None, weights=weights)
-#       weights = weights_broadcast_ops.broadcast_weights(
-#           math_ops.cast(weights, dtypes.float32), values)
-#       values = math_ops.multiply(values, weights)
-#       num_values = math_ops.reduce_sum(weights)
+    def compute_identity(_, t):
+      return math_ops.scalar_mul(1, t, name='value')
 
-#     update_total_op = state_ops.assign_add(total, math_ops.reduce_sum(values))
-#     with ops.control_dependencies([values]):
-#       update_count_op = state_ops.assign_add(count, num_values)
+    identity_t = _aggregate_across_replicas(None, compute_identity, orig_tens)
+    update_op = math_ops.scalar_mul(1, update_total_op, name='update_op')
 
-#     def compute_mean(_, t, c):
-#       return math_ops.div_no_nan(t, math_ops.maximum(c, 0), name='value')
+    return identity_t, update_op
 
-#     mean_t = _aggregate_across_replicas(
-#         metrics_collections, compute_mean, total, count)
-#     update_op = math_ops.div_no_nan(
-#         update_total_op, math_ops.maximum(update_count_op, 0), name='update_op')
-
-#     return mean_t, update_op
 
 class PretrainingModel(object):
   """Transformer pre-training using the replaced-token-detection task."""
@@ -167,9 +160,10 @@ class PretrainingModel(object):
       metrics = dict()
       # new
       utils.log(d["input_ids"])
-      node1 = tf.reshape(d["input_ids"], [-1])
-      node1_print_output = tf.Print(node1, [node1])
-      metrics["dumb"] = tf.metrics.mean(node1_print_output)
+      utils.log(cringe(d["input_ids"]))
+      # node1 = tf.reshape(d["input_ids"], [-1])
+      # node1_print_output = tf.Print(node1, [node1])
+      # metrics["dumb"] = tf.metrics.mean(node1_print_output)
       # utils.log(node1)
       # utils.log(tf.Print(node1, [node1]))
       # utils.log(tf.metrics.accuracy(
